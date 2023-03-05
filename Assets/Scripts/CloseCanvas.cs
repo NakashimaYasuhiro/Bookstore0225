2a14b583f755560dec8cb934c49b37d9c2e23195
nakashima <n09057015617@gmail.com>
2023-03-05 09:40:32 +0900

    Mar05 09:38
    subcam2 have been changed for Zoom each book

---
+++ Assets/Scripts/CloseCanvas.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.Rendering.DebugUI;
+
+public class CloseCanvas : MonoBehaviour
+{
+    [SerializeField] GameObject panel;
+    public ZoomClickedBook cameramanager;
+        
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+       
+
+    }
+
+    public void ClosePanel()
+    {
+        Debug.Log("closepanel worked!");
+        Debug.Log("cameramanager.cloneObject:"+cameramanager.cloneObject);   
+        Destroy(cameramanager.cloneObject);
+        
+        panel.SetActive(false);
+
+       
+        
+    }
+}
+
